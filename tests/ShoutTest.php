59fdbeb5eb80aeec85fd3eeaf9f853dc7912ffbd
Deji69 <the_zone@hotmail.co.uk>
2019-10-04 04:26:37 +0100

    Implement limit validation & tests

--- tests/ShoutTest.php
+++ tests/ShoutTest.php
@@ -4,9 +4,41 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ShoutTest extends TestCase
 {
+    const ROUTE = 'api/v1/shout/steve-jobs';
+
     public function testRequestShout()
     {
-        $response = $this->call('GET', 'api/v1/shout/steve-jobs', ['limit' => 1]);
-        $this->assertEquals(200, $response->status());
+        $this->json('GET', self::ROUTE);
+        $this->seeStatusCode(200);
+    }
+
+    public function testShoutValidatesLimit()
+    {
+        // invalidates if provided limit is less than 1
+        $this->json('GET', self::ROUTE, ['limit' => 0]);
+        $this->seeStatusCode(422);
+        $this->seeJson([
+            'errors' => [
+                'limit' => ['The limit must be at least 1.']
+            ]
+        ]);
+
+        // invalidates if provided limit is more than 10
+        $this->json('GET', self::ROUTE, ['limit' => 11]);
+        $this->seeStatusCode(422);
+        $this->seeJson([
+            'errors' => [
+                'limit' => ['The limit may not be greater than 10.']
+            ]
+        ]);
+
+        // invalidates if provided limit is not a number
+        $this->json('GET', self::ROUTE, ['limit' => 'a']);
+        $this->seeStatusCode(422);
+        $this->seeJson([
+            'errors' => [
+                'limit' => ['The limit must be a number.']
+            ]
+        ]);
     }
 }
